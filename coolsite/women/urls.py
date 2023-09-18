1b82db9b58ea8812a6006834f1dbde9659bd69c6
User <r121221@yandex.ru>
2023-09-18 15:21:50 +0300

    сделать список группы страницу и просмотр каждого хранить фио и эмодзи

---
+++ coolsite/women/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index),
+    path('about/', about),
+    path('pri/<int:number_student>/', pri_id),
+    path('pri/<slug:cat>/', categories),
+
+
+]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return HttpResponse('страница приложения women/')
+
+
+def about(request):
+    return HttpResponse('<h1> БГИТУ </h1>')
+
+
+def pri_id(request, number_student):
+    return HttpResponse(f'<h1> ПРИ 201 </h1> <p>Студент под номером {number_student} </p>')
+
+
+def categories(request, cat):
+    return HttpResponse(f'<h1> Ссылка {cat} </h1>')
